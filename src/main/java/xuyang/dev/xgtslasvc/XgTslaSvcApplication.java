b688863c1e31530f1f5350eccb46d34a4684f95a
xgao <xuyang.gao@medimpact.com>
2026-04-01 18:07:30 -0700

    feat: initial project

---
+++ src/main/java/xuyang/dev/xgtslasvc/XgTslaSvcApplication.java
@@ -0,0 +1,15 @@
+package xuyang.dev.xgtslasvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class XgTslaSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XgTslaSvcApplication.class, args);
+    }
+
+}
---
+++ src/main/java/xuyang/dev/xgtslasvc/web/WebController.java
@@ -0,0 +1,44 @@
+package xuyang.dev.xgtslasvc.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@CrossOrigin
+public class WebController {
+
+    @Value("${app.env}")
+    private String apiEnv;
+    private BuildProperties buildProperties;
+
+    public WebController(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @GetMapping(value="/sayhello")
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello,This is for xg-tsla-svc");
+    }
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss_SSS");
+
+
+    @GetMapping(value="/health")
+    public ResponseEntity<String> getHealth(){
+        ZonedDateTime nowLA = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        return ResponseEntity.ok( nowLA.format(formatter));
+    }
+
+    @GetMapping(value = "/version")
+    public ResponseEntity<String> getVersion(){
+        return ResponseEntity.ok(apiEnv+":"+buildProperties.getVersion());
+    }
+}
